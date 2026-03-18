3427f3a06c9bd291e863015a8a62df3ce3797efb
Martin Koldovsky <mkoldov@gmail.com>
2026-03-18 12:55:06 +0000

    py

--- 02-with-dataset.py
+++ 02-with-dataset.py
@@ -6,11 +6,11 @@ import pandas as pd
 import requests
 import os
 
+# dataset from Hugging Face
 df = pd.read_json("hf://datasets/OpenSafetyLab/Salad-Data/base_set.json")
 
-print(df.head())
-
-print(df.columns)
+# print(df.head())
+# print(df.columns)
 
 # select random prompt
 prompt = df.sample(1)["question"].values[0]
@@ -27,6 +27,9 @@ response = session.post(
     headers={"Authorization": f"Bearer {lakera_guard_api_key}"},
 )
 result = response.json()
+
+print(result)
+
 if result.get("flagged"):
     detected = [b for b in result.get("breakdown", []) if b.get("detected")]
     print(f"FLAGGED | {result['metadata']['request_uuid']}")
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Lakera API basics
+
+
+# Prerequisites
+```shell
+curl -sfS https://dotenvx.sh | sudo sh
+dotenvx help
+```
+
+## 2 python
+
+```shell
+uv venv
+uv pip install click pandas requests fsspec huggingface_hub
+
+uv run 02-with-dataset.py
+
+dotenvx run -- uv run 02-with-dataset.py
+
+
+```
